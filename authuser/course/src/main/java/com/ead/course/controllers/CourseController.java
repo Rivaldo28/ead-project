480963969b5ba3d5964586286589639c99d012de
Rivaldo Souza <rivaldo.lsouza@outlook.com>
2025-05-18 17:58:26 -0300

    WIP: salvando alterações antes do rebase

--- authuser/course/src/main/java/com/ead/course/controllers/CourseController.java
+++ authuser/course/src/main/java/com/ead/course/controllers/CourseController.java
@@ -3,13 +3,8 @@ package com.ead.course.controllers;
 import com.ead.course.dtos.CourseDto;
 import com.ead.course.models.CourseModel;
 import com.ead.course.services.CourseService;
-import com.ead.course.specifications.SpecificationTemplate;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,7 +25,7 @@ public class CourseController {
     private CourseService courseService;
 
     @PostMapping
-    public ResponseEntity<Object> saveCourse(@RequestBody @Valid CourseDto courseDto) {
+    public ResponseEntity<Object> saveCourse(@RequestBody @Valid CourseDto courseDto){
         var courseModel = new CourseModel();
         BeanUtils.copyProperties(courseDto, courseModel);
         courseModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
@@ -39,9 +34,9 @@ public class CourseController {
     }
 
     @DeleteMapping("/{courseId}")
-    public ResponseEntity<Object> deleteCourse(@PathVariable(value = "courseId") UUID courseId) {
+    public ResponseEntity<Object> deleteCourse(@PathVariable(value = "courseId")UUID courseId) {
         Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
-        if (!courseModelOptional.isPresent()) {
+        if(!courseModelOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found");
         }
         courseService.delete(courseModelOptional.get());
@@ -52,7 +47,7 @@ public class CourseController {
     public ResponseEntity<Object> updateCourse(@PathVariable(value = "courseId") UUID courseId,
                                                @RequestBody @Valid CourseDto courseDto) {
         Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
-        if (!courseModelOptional.isPresent()) {
+        if(!courseModelOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found");
         }
         var courseModel = courseModelOptional.get();
@@ -66,18 +61,14 @@ public class CourseController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<CourseModel>> getAllCourses(SpecificationTemplate.CourseSpec spec,
-                                                           @PageableDefault(page = 0, size = 10,
-                                                                   sort = "courseId",
-                                                                   direction = Sort.Direction.ASC)
-                                                           Pageable pageable) {
-        return ResponseEntity.status(HttpStatus.OK).body(courseService.findAll(spec,pageable));
+    public ResponseEntity<List<CourseModel>> getAllCourses(){
+        return ResponseEntity.status(HttpStatus.OK).body(courseService.findAll());
     }
 
     @GetMapping("/{courseId}")
-    public ResponseEntity<Object> getOneCourses(@PathVariable(value = "courseId") UUID courseId) {
+    public ResponseEntity<Object> getOneCourses(@PathVariable(value = "courseId") UUID courseId){
         Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
-        if (!courseModelOptional.isPresent()) {
+        if(!courseModelOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found");
         }
         return ResponseEntity.status(HttpStatus.OK).body(courseModelOptional.get());
--- authuser/course/src/main/java/com/ead/course/services/CourseService.java
+++ authuser/course/src/main/java/com/ead/course/services/CourseService.java
@@ -1,10 +1,6 @@
 package com.ead.course.services;
 
 import com.ead.course.models.CourseModel;
-import com.ead.course.specifications.SpecificationTemplate;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,5 +14,5 @@ public interface CourseService {
 
     Optional<CourseModel> findById(UUID courseId);
 
-    Page<CourseModel> findAll(Specification<CourseModel> spec, Pageable pageable);
+    List<CourseModel> findAll();
 }
