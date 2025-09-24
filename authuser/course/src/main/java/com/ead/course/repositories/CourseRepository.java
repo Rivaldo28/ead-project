5fb065db5dd51f38a39d028edc48ec6014f56d32
Rivaldo Souza <rivaldo.lsouza@outlook.com>
2025-09-24 08:10:39 -0300

    limpando sujeiras

--- authuser/course/src/main/java/com/ead/course/repositories/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ead.course.repositories;
-
-import com.ead.course.models.CourseModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CourseRepository extends JpaRepository<CourseModel, UUID> {
-}
